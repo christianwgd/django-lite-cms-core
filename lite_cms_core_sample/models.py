6392bdb333804db58f5ed3ba1f1c352f4d13c230
Christian Wiegand <christianwgd@users.noreply.github.com>
2024-11-25 22:31:39 +0100

    Fix get_slug method signature

--- lite_cms_core_sample/models.py
+++ lite_cms_core_sample/models.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.urls import reverse
-from django.utils.text import slugify
 
 from lite_cms_core.models import BaseEntity, SluggedMixin
 
@@ -31,5 +30,4 @@ class SluggedItemFromName(SluggedMixin, BaseItem):
     name = models.CharField(max_length=100)
 
     def get_slug(self, attr=None):
-        attr = "name"
-        return slugify(getattr(self, attr, None) or self.name)
+        return super().get_slug(attr="name")
